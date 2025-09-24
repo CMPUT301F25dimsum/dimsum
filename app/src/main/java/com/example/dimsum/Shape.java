4722805ac5767687c8657607e3e5800a400e320b
Cosmic-G-G <140998316+Cosmic-G-G@users.noreply.github.com>
2025-09-24 17:44:51 -0600

    Change to master

--- app/src/main/java/com/example/dimsum/Shape.java
+++ app/src/main/java/com/example/dimsum/Shape.java
@@ -3,6 +3,7 @@ package com.example.dimsum;
 public abstract class Shape {
     private int x;
     private int y;
+    String color = "cyan";
 
     public Shape(int x, int y) {
         this.x = x;
