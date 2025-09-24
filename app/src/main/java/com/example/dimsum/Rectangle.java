8fb81e63bf8b9064d2226294d49806dd815ee5ea
koim ian <xindi8@ug16.ualberta.ca>
2025-09-24 16:55:06 -0600

    lab 4

---
+++ app/src/main/java/com/example/dimsum/Rectangle.java
@@ -0,0 +1,37 @@
+package com.example.dimsum;
+
+public class Rectangle extends Shape {
+    private float length;
+    private float width;
+
+    public Rectangle(int x, int y, float length, float width) {
+        super(x, y);           // call Shape constructor
+        this.length = length;  // initialize Rectangle fields
+        this.width = width;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    // Example of extending behavior
+    public float getArea() {
+        return length * width;
+    }
+
+    public float getPerimeter() {
+        return 2 * (length + width);
+    }
+}
