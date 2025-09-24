4f8a3126dcc38140f0532997dd2d7fb68fcdb237
John Alvaro <john.d.alvaro@gmail.com>
2025-09-24 17:06:37 -0600

    created string field within Shape to generate merge conflict

--- app/src/main/java/com/example/dimsum/Rhombus.java
+++ app/src/main/java/com/example/dimsum/Rhombus.java
@@ -6,7 +6,6 @@ public class Rhombus extends Shape {
     public Rhombus(int x, int y) {
         super(x,y);
     }
-
     public void printStr() {
         System.out.println("I am a rhombus.");
     }
